2a661b3c9b3519523235c912c07b50d798df2f04
JavelinSX <zuev499@gmail.com>
2024-12-23 20:51:08 +0300

    filter tech

--- src/components/CardProject.vue
+++ src/components/CardProject.vue
@@ -1,33 +1,107 @@
 <template>
-  <v-row align-center justify-center justify-space-between v-for="project in projects" :key="project.name">
+  <!-- Filters -->
+  <v-row v-if="selectedTechnologies.length">
+    <v-col cols="12" sm="4" md="6">
+      <v-card class="m-4" title="Фильтр по технологиям">
+        <v-card-item>
+          <v-chip v-for="tech in selectedTechnologies" :key="tech.name" :color="tech.color" class="p-4" closable
+            @click:close="removeTechnologyFilter(tech)">
+            <v-icon start>{{ tech.icon }}</v-icon>
+            {{ tech.name }}
+          </v-chip>
+        </v-card-item>
+      </v-card>
+    </v-col>
+  </v-row>
+
+  <!-- Projects -->
+  <v-row v-for="project in filteredProjects" :key="project.name" align-center justify-center justify-space-between>
     <v-col cols="12" sm="4" md="6">
       <v-card class="m-4 flex flex-col md:flex-row" :title="project.name" :subtitle="project.type" elevation="10">
-        <v-card-text class="flex flex-col md:flex-row">
+        <v-card-text class="flex flex-col md:flex-row min-h-[300px]">
 
-          <div class="flex gap-2 flex-wrap max-w-80">
+          <div class="flex gap-2 flex-wrap max-w-80 self-start">
             <p class="m-4 w-full">{{ project.description }}</p>
-            <v-chip v-for="tech in project.technologies" :key="tech.name" :color="tech.color">
+            <v-chip v-for="tech in project.technologies" :key="tech.name" :color="tech.color"
+              @click="addTechnologyFilter(tech)">
               <v-icon start>{{ tech.icon }}</v-icon>
               {{ tech.name }}
             </v-chip>
           </div>
-          <v-divider class="mt-3 mb-2"></v-divider>
-          <div class="w-full w-80">
-            <v-card append-icon="mdi-open-in-new" class="m-2" :href="project.github" prepend-icon="mdi-github"
-              rel="noopener" subtitle="Репозиторий с исходниками" target="_blank" title="GitHub"></v-card>
-            <v-card append-icon="mdi-open-in-new" class="m-2" :href="project.live" prepend-icon="mdi-github"
-              rel="noopener" subtitle="Прод. версия" target="_blank" title="Production"></v-card>
+
+          <divider-main orientation="horizontal" class="mt-3 mb-2 md:hidden" />
+          <divider-main orientation="vertical" class="ml-2 mr-2 hidden md:block" />
+
+          <div class="w-80">
+            <v-card v-for="link in projectLinks(project)" :key="link.title" v-bind="link" class="m-2"
+              append-icon="mdi-open-in-new" prepend-icon="mdi-github" rel="noopener" target="_blank" />
           </div>
         </v-card-text>
-
-
       </v-card>
     </v-col>
   </v-row>
 </template>
 
 <script setup lang="ts">
+import { computed, ref } from 'vue'
 import data from '../assets/data.json'
-const projects = data.projects
-const skills = data.skills
+import DividerMain from './DividerMain.vue'
+
+interface Tech {
+  name: string
+  color: string
+  icon: string
+}
+
+interface Project {
+  name: string
+  type: string
+  description: string
+  technologies: Tech[]
+  github: string
+  live: string
+}
+
+const originalProjects = ref<Project[]>(data.projects)
+const selectedTechnologies = ref<Tech[]>([])
+
+const filteredProjects = computed(() => {
+  if (selectedTechnologies.value.length === 0) {
+    return originalProjects.value
+  }
+
+  return originalProjects.value.filter((project) => {
+    return project.technologies.some((projectTech) => {
+      return selectedTechnologies.value.some(
+        (selectedTech) => selectedTech.name === projectTech.name
+      )
+    })
+  })
+})
+
+const projectLinks = (project: Project) => [
+  {
+    href: project.github,
+    title: 'GitHub',
+    subtitle: 'Репозиторий с исходниками'
+  },
+  {
+    href: project.live,
+    title: 'Production',
+    subtitle: 'Прод. версия'
+  }
+]
+
+const addTechnologyFilter = (tech: Tech) => {
+  const exists = selectedTechnologies.value.some((t) => t.name === tech.name)
+  if (!exists) {
+    selectedTechnologies.value = [...selectedTechnologies.value, tech]
+  }
+}
+
+const removeTechnologyFilter = (tech: Tech) => {
+  selectedTechnologies.value = selectedTechnologies.value.filter(
+    (t) => t.name !== tech.name
+  )
+}
 </script>
---
+++ src/components/DividerMain.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-full opacity-10 bg-slate-800"
+    :class="props.orientation === 'vertical' ? ['self-stretch w-[1px]'] : ['w-full h-[1px]']">
+  </div>
+</template>
+<script setup lang="ts">
+const props = defineProps(
+  {
+    orientation: String,
+  }
+)
+</script>
